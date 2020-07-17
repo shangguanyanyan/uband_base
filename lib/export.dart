6dfd9e8393c32aff39eea3a65241758401707067
shangguanyanyan <1024503335@qq.com>
2020-07-17 15:41:53 +0800

    升级flutter_html

--- lib/export.dart
+++ lib/export.dart
@@ -3,4 +3,5 @@ export 'package:cookie_jar/cookie_jar.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:flutter_html/flutter_html.dart';
 export 'package:flutter_html/html_parser.dart';
-export 'package:flutter_html/image_properties.dart';
+export 'package:flutter_html/style.dart';
+
