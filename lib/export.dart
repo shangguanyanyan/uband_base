e9907bb6d756e1bac8da946889f5deef7c726158
shangguanyanyan <1024503335@qq.com>
2020-08-31 14:47:23 +0800

    export custom dialog

--- lib/export.dart
+++ lib/export.dart
@@ -1,4 +1,4 @@
 export 'widget/widget.dart';
 export 'package:cookie_jar/cookie_jar.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
-
+export 'package:flutter_custom_dialog/flutter_custom_dialog.dart';
